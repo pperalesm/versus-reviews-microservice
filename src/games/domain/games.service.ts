ad0fc3111b6303525ad2794a53273257e18edbd5
Pablo Perales Molas <pperalesm@gmail.com>
2022-06-25 16:59:42 +0200

    Added missing await

--- src/games/domain/games.service.ts
+++ src/games/domain/games.service.ts
@@ -11,19 +11,23 @@ export class GamesService {
   }
 
   async handleCreated(title: string, timestamp: string) {
-    return await this.gamesRepository.handleCreate(
+    await this.gamesRepository.handleCreate(
       new Game({ title: title }),
       timestamp,
     );
   }
 
   async handleDeleted(title: string, timestamp: string) {
-    return await this.gamesRepository.handleDelete({ title: title }, timestamp);
+    await this.gamesRepository.handleDelete({ title: title }, timestamp);
   }
 
   async handleUpdated(oldTitle: string, newTitle: string, timestamp: string) {
     if (oldTitle != newTitle) {
-      this.gamesRepository.handleTitleChange(oldTitle, newTitle, timestamp);
+      await this.gamesRepository.handleTitleChange(
+        oldTitle,
+        newTitle,
+        timestamp,
+      );
     }
   }
 }
